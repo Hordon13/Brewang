7a3aace4c8a86dea46e561e9b0c8f218a3602a4a
Horváth Donát <hordon@hordon.local>
2019-12-01 10:54:41 +0100

    Init project: add components, routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BrewListComponent } from './components/brew-list/brew-list.component';
+import {BrewDetailComponent} from './components/brew-detail/brew-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'breweries', component: BrewListComponent},
+  {path: 'brewery/:id', component: BrewDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [BrewListComponent, BrewDetailComponent];
